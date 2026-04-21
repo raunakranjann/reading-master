5ccfff676ffd3ed0177616e167ebdedebe7fc519
Raunak Ranjan <raunakranjann@gmail.com>
2026-04-22 00:26:50 +0530

    Initial Commit

---
+++ src/main/java/com/readingmaster/ReadingMasterApplication.java
@@ -0,0 +1,13 @@
+package com.readingmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadingMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadingMasterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/readingmaster/ReadingMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.readingmaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadingMasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
